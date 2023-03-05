591015482538f8678a25cecc1ef727127854277c
Abhinav Kumar <31796348+DreadDeveloper@users.noreply.github.com>
2023-03-05 10:57:46 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Main {
+interface Lamb
+{
+	void display();
+}
+interface Da
+{
+	void display(String firstName, String secondName);
+}
+/*class DisLamb implements Lamb
+{
+	public void display()
+	{
+		System.out.println("This is the interface.");
+	}
+}*/
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Lamb lamb = () -> System.out.println("This is the interface.");
+		Da da = (firstName, secondName) -> System.out.println("This is "+ firstName + " " + secondName + ".");
+		lamb.display();
+		da.display("John", "Doe");
+	}
 }
